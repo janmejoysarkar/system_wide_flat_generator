af41c56f27bbddecaaef3b28f2c296ad801bcf98
janmejoy <janmejoysarker@gmail.com>
2024-05-19 01:51:19 +0530

    Code paralellized to process multiple filters simultaneously

--- flat_generator.py
+++ flat_generator.py
@@ -14,6 +14,7 @@ import numpy as np
 from astropy.convolution import convolve
 from astropy.convolution import Box2DKernel
 import os
+from concurrent.futures import ProcessPoolExecutor
 
 def blur(data, kernel): #blurring function
     return(convolve(data, Box2DKernel(kernel), normalize_kernel=True))
@@ -23,7 +24,7 @@ def lighten(image_list):
     Lighten blend
     image_list= List of 2D numpy arrays
     '''
-    lighten_blend=np.zeros(np.shape(data))
+    lighten_blend=np.zeros(np.shape(image_list[0]))
     for image in image_list:
         for i in range(4096):
             for j in range(4096):
@@ -42,7 +43,7 @@ def calib_status(data, row, col, size):
     std_pc, mean= round(std*100/mean, 2), round(mean, 2)
     return(std_pc, mean)
 
-def profile(data, row, col): 
+def profile(ftrname, data, row, col): 
     '''
     To plot the image and generate line_profile of any numpy 2D Array
     data= 2D numpy array
@@ -65,7 +66,7 @@ def profile(data, row, col):
         ax.grid() 
     plt.show()
     
-def prep_header(mfg, data_date):
+def prep_header(ftrname, mfg, data_date):
     header=fits.Header()
     header['VERSION']=('beta', 'Version name for the Flat Field')
     header['FTR_NAME']=(ftrname, 'Filter Name for SUIT')
@@ -80,21 +81,12 @@ def plot(data, caption): #plots any numpy array with imshow (caption= image titl
     plt.title(caption)
     plt.show()
 
-
-if __name__=='__main__':
-    
-    ftr_list= ["NB01", "NB02", "NB03", "NB04", "NB05", "NB06", 
-               "NB07", "NB08", "BB01", "BB02", "BB03"]
-    #### USER-DEFINED ####
-    ftrname="NB08"
+def flat_generator(ftrname):
     project_path= os.path.expanduser('~/Dropbox/Janmejoy_SUIT_Dropbox/flat_field/system_wide_flat_project/')
     folder=project_path+'data/processed/'+ftrname+'/masked_scatter_corrected_averaged_files/'
-    sav= project_path+'products/shtr_0_reduced_avg_files_flat/'
+    sav= project_path+'products/shtr_0_reduced_avg_files_flat_lvl1/'
     #thres=0
-    save= True #toggle to False to not save the image
     savename= "masked_scatter_corrected_reduced_avg_files_flat"
-    shtr= "0" #to be used in saved filename
-    head=prep_header('2024-05-19', '2024-01-29')
     #######################
     
     print("Generating Flat for", ftrname)
@@ -113,12 +105,12 @@ if __name__=='__main__':
     flat_field= blur(small_scale_removed_img/large_scale_img, 100) 
     flat_field_lvl1= np.transpose(flat_field)
     
-    hdu= fits.PrimaryHDU(flat_field_lvl1, header=head)
+    hdu= fits.PrimaryHDU(flat_field_lvl1, header=prep_header(ftrname, mfg_date, data_date))
     #saves the fits file
-    if save==True : hdu.writeto(sav+ftrname+'_shtr_'+shtr+"_"+savename+'_lvl1.fits', flat_field_lvl1, overwrite=True)
+    if save==True : hdu.writeto(sav+ftrname+'_shtr_'+shtr+"_"+savename+'_lvl1.fits', overwrite=True)
     
     #visualization
-    profile(flat_field, 2048, 2048) #to plot image profile
+    profile(ftrname, flat_field, 2048, 2048) #to plot image profile
 
     #statistics
     print("Pos(200px Box) \t %Std, Mean")
@@ -126,3 +118,16 @@ if __name__=='__main__':
     print("(2000, 2000)",calib_status(flat_field, 2000, 2000, 100))
     print("(3000, 3000)",calib_status(flat_field, 3000, 3000, 100))
     
+    
+if __name__=='__main__':
+
+    #### USER-DEFINED ####
+    #ftrname="NB08"
+    mfg_date, data_date= '2024-05-19', '2024-01-29'
+    save= True #toggle to False to not save the image
+    shtr= "0" #to be used in saved filename
+    ftr_list= ["NB01", "NB02", "NB03", "NB04", "NB05", "NB06", 
+               "NB07", "NB08", "BB01", "BB02", "BB03"]
+    
+    with ProcessPoolExecutor() as execute:
+        execute.map(flat_generator, ftr_list)
